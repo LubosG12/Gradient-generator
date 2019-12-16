2a1b7e59f0a6ae670fd68e0ad1ab83e7174464f8
Lubomir <58482574+LubosG12@users.noreply.github.com>
2019-12-16 17:20:38 +0100

    Added randomize button

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById('gradient');
 var css2 = document.getElementById('copyStatus');
+var randomize = document.getElementById('randomize');
 
 function updateBackground() {
     body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
@@ -21,7 +22,22 @@ function copy() {
    css2.textContent = "Copied to clipboard!";
 }
 
+function getRandomColor() {
+  var letters = "0123456789ABCDEF";
+  color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
 
+randomize.addEventListener("click", function() {
+  getRandomColor();
+  color1.value = color;
+  getRandomColor();
+  color2.value = color;
+  updateBackground();
+});
 
 css.addEventListener("click", copy);
 color1.addEventListener("input", updateBackground);
